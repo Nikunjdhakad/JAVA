9ab79696a369eef90057a4e40f25698cb2c0926f
Nikunj Dhakad <nikunjdhakad@gmail.com>
2025-11-24 20:25:46 +0530

    Ternary__24-11-25......

---
+++ ternary.java
@@ -0,0 +1,18 @@
+public class ternary{
+       public static void main(String[]args){
+        int P= 6;
+        int R= 0;
+      //  if (P%2==0)
+        // R = 7;
+         //else
+          // R =9;
+
+          // ?:     ____ternary___
+
+          R = P%2==0 ? 7 : 9;
+
+
+           System.out.println(R);
+
+       }
+}
