7a71b88c408d0a16224e8b27203b22655638c85e
Nikunj Dhakad <nikunjdhakad@gmail.com>
2025-11-25 08:23:29 +0530

    While Loop .........

---
+++ whileloop.java
@@ -0,0 +1,13 @@
+public class whileloop{
+    public static void main(String[]args){
+        
+        int i = 1;
+
+      while(i<=7)  
+        {
+            System.out.println( i + "__Jai Shree Ram__");
+
+            i++;
+        }
+    }
+}
