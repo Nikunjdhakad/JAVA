953bd6fa0854fc895c597a2964ff6df8f516df29
Nikunj Dhakad <nikunjdhakad@gmail.com>
2025-11-26 23:51:49 +0530

    Add files via upload

---
+++ switchcase.java
@@ -0,0 +1,32 @@
+public class switchcase {
+    public static void main(String [] args){
+        int p = 5;
+
+        switch(p){
+            case 1:
+                  System.out.println("monday");
+                  break;
+            case 2:
+                  System.out.println("tuesday");
+                  break;
+            case 3:
+                  System.out.println("wednessday");   
+                  break;   
+            case 4:
+                  System.out.println("thursday");
+                  break;
+            case 5:
+                  System.out.println("friday");
+                  break;
+            case 6:
+                  System.out.println("saturday");
+                  break;
+            case 7:
+                  System.out.println("Sunday");
+            
+            default:
+                System.out.println("Not awailable");
+                  
+        }
+    }
+}
