82234c292cacdffac0cfd3dedeac34424f2a67f4
Nikunj Dhakad <nikunjdhakad@gmail.com>
2025-11-28 20:43:39 +0530

    Add files via upload

---
+++ ao.java
@@ -0,0 +1,13 @@
+// ao :- arthmetic operators ::--  [-,+,*,%,/]
+
+public class ao{
+    public static void main(String args [])
+    {
+       int A =74566;
+        //int B =9;
+        //int result = (A % B) ;
+        //int  D = ( A / B );
+         int R =  A++;
+        System.out.println( R);
+    }
+}
