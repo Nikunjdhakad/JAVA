f9008ac8ebb88936c624ac4649cf5272ac0e421a
Nikunj <nikunjdhakad@gmail.com>
2026-03-03 21:52:38 +0530

    new file added

---
+++ claobj.java
@@ -0,0 +1,15 @@
+class calculator 
+{
+    int X;
+    return 0;
+}
+
+
+public class claobj {
+    public static void main(String arg[]){
+        int num1 = 4;
+        int num2 = 7;
+
+
+    }
+}
