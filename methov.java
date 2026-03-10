6c52ea5730df3876818efc76a569d3e06aa0d38c
Nikunj <nikunjdhakad@gmail.com>
2026-03-10 19:22:11 +0530

    Method overloading concepts uderstanding on basic levels

---
+++ methov.java
@@ -0,0 +1,35 @@
+
+//* This code is for method overloading consepts */
+
+
+
+class overloading{
+  
+   public  int add (int n1, int n2 )
+{
+    return n1+n2;
+  }
+ 
+   public int add (int n1, int n2, int n3)
+   {
+    return n1+n2+n3;
+   }
+
+   public double add(double n1, int n2, int n3)
+   {
+    return n1+n2+n3;
+   }
+
+    
+}
+public class methov {
+    public static void main(String args [])
+    {
+       overloading plus = new overloading();
+
+       double r = plus.add(5.2,21,6);
+       System.out.println(r);
+      
+    }
+
+}
