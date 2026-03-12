55101c80eb976a4d28db6f5565d2217dd4de691b
Nikunj <nikunjdhakad@gmail.com>
2026-03-12 18:05:05 +0530

    Creation of array and basic understanding

---
+++ array.java
@@ -0,0 +1,27 @@
+public class array {
+    public static void main(String arg[])
+    {
+        int num[] = {2,8,9};
+
+        for (int i=0; i <3; i++)
+        {
+            System.out.println(num[i]);
+        }
+
+
+        int num1[]= new int [5];
+        num1[0]= 9;
+        num1[1]= 8;
+        num1[2]= 7;
+        num1[3]= 6;
+        num1[4]= 5;
+
+        for(int i=0; i<5; i++)
+        {
+            System.out.println(num1[i]);
+        }
+
+        
+    }
+    
+}
