0c62a1aad1cd4a9c7d5191fc3e7cc595535b759e
Nikunj <nikunjdhakad@gmail.com>
2026-03-13 07:19:32 +0530

    Two dimensional array

--- twoD.java
+++ twoD.java
@@ -3,6 +3,16 @@ public class twoD {
     {
         int nums [] [] = new int [3] [4]; 
 
+        int random = 0;
+
+        for(int i=0; i<3; i++)
+        {
+            for(int j=0; j<4; j++)
+            {
+                 nums [i][j]= (int)( Math.random() * 10);
+            }
+        }
+
         for (int i=0; i<3; i++ )
         {
             for(int j=0; j<4; j++)
@@ -11,5 +21,22 @@ public class twoD {
             }
             System.out.println();
         }
+
+        // for ( n[]:nums)
+        // {
+        //     for n=
+        // }
+        for( int n[] : nums)
+        {
+            for(int m:n)
+            {
+                System.out.print(m+"   ");
+            }
+            System.out.println( );
+        
+        }
+        
+    
+
     }
 }
