6ddaf6c316e10bf797db17965cc65db0b1bf2b0a
Nikunj Dhakad <nikunjdhakad@gmail.com>
2026-03-16 19:43:49 +0530

    Jagged array concept

---
+++ jagged.java
@@ -0,0 +1,29 @@
+public class jagged {
+    public static void main(String a[])
+    {
+        int nums [][]=new int [4][];
+
+        nums[0]= new int [11];
+        nums[1]= new int [12];
+        nums[2]= new int [8];
+        nums[3]= new int [7];
+
+        for(int i=0; i<nums.length; i ++)
+        {
+            for(int j=0; j<nums[i].length; j++)
+            {
+                nums[i][j]= (int)(Math.random() *10);
+            }
+
+        }
+
+        for (int n[] : nums)
+        {
+            for(int m:n)
+            {
+                System.out.print(m+"  ");
+            }
+            System.out.println();
+        }
+    }
+}
