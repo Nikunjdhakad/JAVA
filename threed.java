b2935b5ad2b53bc018ba6f900670de35530286c1
Nikunj Dhakad <nikunjdhakad@gmail.com>
2026-03-16 20:01:47 +0530

    three dimensional array code and some exercise

---
+++ threed.java
@@ -0,0 +1,31 @@
+public class threed {
+    public static void main (String a[])
+    {
+        int arr[][][]= new int [5][5][];
+
+        for(int i=0; i<arr.length; i++)
+        {
+            for(int j=0; j<arr.length; j++)
+            {
+                for(int k=0; k<arr.length; k++)
+                {
+                    arr[i][j][k]=(int)(Math.random() *10);
+                }
+            }
+        }
+
+         for(int[][] n : arr)
+        {
+            for(int[] m : n)
+            {
+                for(int x : m)
+                {
+                    System.out.print(x + " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+
+    }
+}
