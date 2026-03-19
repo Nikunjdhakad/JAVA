ff3abef0db7081f2cf87158a227d70f712602c0b
Nikunj Dhakad <nikunjdhakad@gmail.com>
2026-03-19 10:45:47 +0530

    array of objects 2nd time

---
+++ ob.java
@@ -0,0 +1,51 @@
+class hostle
+{
+    int roomno;
+    String type;
+    int bad;
+}
+
+
+public class ob {
+    public static void main(String a[])
+    {
+        hostle host =new hostle();
+
+        host.roomno =20;
+        host.type = "'boys'";
+        host.bad = 3;
+
+
+        hostle host1 = new hostle();
+        host1.roomno = 21;
+        host1.type = "girls";
+        host1.bad =3;
+
+
+        
+        hostle host2 = new hostle();
+        host2.roomno = 23;
+        host2.type = " senior girls";
+        host2.bad =5;
+
+        
+        hostle host3 = new hostle();
+        host3.roomno = 420;
+        host3.type = "senior boys";
+        host3.bad =6;
+
+        hostle hostles[]= new hostle[4];
+
+        hostles[0]= host;
+        hostles[1]= host1;
+        hostles[2]= host2;
+        hostles[3]= host3;
+
+        for(hostle H : hostles)
+        {
+            System.out.println(H.roomno +" : " + H.type + " : " + H.bad);
+        }
+
+
+    }
+}
