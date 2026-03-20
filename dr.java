45f546972f45c631a4ad5d99fd7ea7b51d75acf4
Nikunj Dhakad <nikunjdhakad@gmail.com>
2026-03-20 19:05:20 +0530

    Working with Arrays in Java: From Basics to Enhanced For Loops

---
+++ dr.java
@@ -0,0 +1,15 @@
+public class dr {
+    public static void main(String[] args) {
+        
+        // TODO: Declare and initialize an integer array with the values 45, 22, 89, 16, 90, and 33
+      
+      
+        int[] numbers = {45, 22, 89, 16, 90, 33};
+
+        // TODO: Initialize min and max with the first element
+
+        // TODO: Use an enhanced for loop to find min and max
+
+        // TODO: Print the smallest and largest numbers
+    }
+}
