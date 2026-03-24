688580c11bdb7c937520eabe684578cd531a696c
Nikunj Dhakad <nikunjdhakad@gmail.com>
2026-03-24 21:00:56 +0530

    type casting of the value's

--- casting.java
+++ casting.java
@@ -2,7 +2,7 @@ public class casting {
     public static void main(String args[])
        {
      
-      byte C1 = 8, C2 = 20;
+      byte C1 = 45, C2 = 20;
       byte C3= (byte) (C1 + C2);
       System.out.println(C3);
 
