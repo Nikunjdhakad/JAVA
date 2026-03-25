2c12c697282caf285afa277f70f5cb14e795bd1c
Nikunj Dhakad <nikunjdhakad@gmail.com>
2026-03-25 13:17:15 +0530

    Simple code of String Buffer for practice

---
+++ strb.java
@@ -0,0 +1,14 @@
+public class strb {
+    public static void main(String[]a)
+    {
+        StringBuffer temple = new StringBuffer("Ayodhya ");
+
+        temple.append( " Ram ");
+        temple.insert(0,"Shree ");
+        //temple.setLength();
+        System.out.println(temple);
+        temple.setLength(50); //this line set the new length or capacity of the string......
+        System.out.println(temple.capacity());
+
+    }
+}
