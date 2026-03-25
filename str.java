22ba12c266460d2cd784bd78c97e4dadab45b9db
Nikunj Dhakad <nikunjdhakad@gmail.com>
2026-03-25 08:16:38 +0530

    String first code practice

---
+++ str.java
@@ -0,0 +1,9 @@
+public class str {
+    public static void main (String []args){
+        String God = "RAM";
+        String Devotee = new String("HANUMAN");
+
+        System.out.println(God);
+        System.out.println(Devotee);
+    }
+}
